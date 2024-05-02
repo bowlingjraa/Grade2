c4a2bd87f84ce32fa4f24f2146093da4cd2e34dd
bowlingjraa <chenizjii@gmail.com>
2024-05-02 14:46:54 +0700

    Input number

--- src/Main.java
+++ src/Main.java
@@ -6,19 +6,19 @@ public class Main {
         System.out.println("Input number");
         int number = scanner.nextInt();
 
-        if (number <= 49) {
+        if (number < 49) {
             System.out.println("grade F" + number);
         }
-        else if (number > 49 && number <= 60){
+        else if (number > 49 && number < 60){
             System.out.println("grade D" + number);
         }
-        else if (number > 60 && number <= 70) {
+        else if (number > 60 && number < 70) {
             System.out.println("grade C" + number);
         }
-        else if (number > 70 && number <= 80) {
+        else if (number > 70 && number < 80) {
             System.out.println("grade B" + number);
         }
-        else if (number > 80 && number <= 100) {
+        else if (number > 80 && number < 100) {
             System.out.println("grade A" + number);
         }
     }
